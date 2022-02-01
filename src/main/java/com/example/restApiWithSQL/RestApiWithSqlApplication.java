a749139f6e78f3408c1ff37201e0b7a5560cc5e0
Viktor <DarmokhvalViktor@gmail.com>
2022-02-01 17:32:40 +0200

    .

---
+++ src/main/java/com/example/restApiWithSQL/RestApiWithSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.restApiWithSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restApiWithSQL/RestApiWithSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restApiWithSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
